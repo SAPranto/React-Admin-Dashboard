ffdac7e105ffb76a6d682fcd8986dc789a736d46
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-04 19:22:30 +0600

    Pages Created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
+import { FiDivideSquare, FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { Navbar, Footer, Sidebar } from './components';
+import { Ecommerce, Orders, Employees, Stacked, Pyramid, Customers, Line, Area, Bar, Pie, Financial, ColorMapping} from './pages';
+import './App.css';
 
 const App = () => {
   return (
