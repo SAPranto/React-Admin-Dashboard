f498bf74c46ce5769b252ec12d85ac469f57e4bf
Shihab Ahmed Pranto <sa.pranto16@gmail.com>
2023-01-05 17:35:29 +0600

    View Products Page Created

---
+++ src/pages/Viewproducts.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable */
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
+const ViewProducts = () => {
+  const selectionsettings = { persistSelection: true };
+  const toolbarOptions = ['Delete'];
+  const editing = { allowDeleting: true, allowEditing: true };
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+    <Header category="Page" title="Customers" />
+    <GridComponent
+      dataSource={employeesData}
+      enableHover={false}
+      allowPaging
+      pageSettings={{ pageCount: 5 }}
+      selectionSettings={selectionsettings}
+      toolbar={toolbarOptions}
+      editSettings={editing}
+      allowSorting
+    >
+      <ColumnsDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {employeesGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+      </ColumnsDirective>
+      <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
+    </GridComponent>
+  </div>
+  )
+}
+
+export default ViewProducts
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,4 +1,6 @@
 export { default as Ecommerce } from './Ecommerce';
 export { default as Orders } from './Orders';
+export { default as Login } from './Login';
 export { default as Customers } from './Customers';
+export { default as Viewproducts } from './Viewproducts';
 export { default as AddProduct } from './AddProduct';
